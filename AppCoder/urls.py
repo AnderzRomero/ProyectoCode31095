0214fe25983cc3499eb24e448e56e27682e6107f
ManfredRomero <mromerorico@uniminuto.edu.co>
2022-09-03 12:59:54 -0500

    Creando mi primera plantilla

---
+++ AppCoder/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from AppCoder.views import curso,entregable,inicio
+
+urlpatterns = [
+    path('', inicio),
+    path('curso/', curso, name='AppCoderCurso'),
+    path('entregable/', entregable, name='AppCoderEntregable')
+]
+
